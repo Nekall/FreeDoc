5115ebf2c5a47d22eae2bc5e9a7a6cd6a4f37029
Neka <nekoanchor@yahoo.fr>
2021-02-04 20:14:34 +0100

    ajout exo v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Doctor.destroy_all
+Patient.destroy_all
+Appointment.destroy_all
+City.destroy_all
+
+50.times do
+  city = City.create([{cities: Faker::Address.city}])
+end
+
+8.times do
+  Doctor.create([{
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  specialty: ["allergologie", "andrologie", "anesthésiologie", "cardiologie", "chirurgie cardiaque", "chirurgie plastique", "chirurgie générale", "chirurgie gynécologique", "chirurgie maxillo-faciale", "chirurgie oculaire", "chirurgie orale", "chirurgie pédiatrique", "chirurgie thoracique", "chirurgie vasculaire", "chirurgie viscérale", "neurochirurgie", "dermatologie", "endocrinologie", "gastro-entérologie", "gériatrie", "gynécologie", "hématologie", "hépatologie", "immunologie", "infectiologie", "médecine générale", "médecine palliative", "néonatologie", "néphrologie", "neurologie", "obstétrique", "odontologie", "oncologie", "ophtalmologie", "orthopédie", "otorhinolaryngologie", "pédiatrie", "pneumologie", "podologie", "psychiatrie", "radiologie", "radiothérapie", "rhumatologie", "urologie"].sample,
+  zip_code: Faker::Number.number(digits: 5),
+  city_id: City.all.sample.id
+  }])
+end
+
+35.times do
+  Patient.create([{
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  city_id: City.all.sample.id
+  }])
+end
+
+50.times do
+  Appointment.create([{
+  date: Faker::Time.forward(days: 90, period: :day),
+  doctor_id: Doctor.all.sample.id,
+  patient_id: Patient.all.sample.id,
+  city_id: City.all.sample.id
+  }])
+end
+
+puts "Patients et Docteurs et RDV créés"
